944b1c0062033dc2e6a4cb04291a4619ecdd89e9
Tran Manh Khai <khai.tran-manh@internal.banvien.com>
2022-09-26 17:07:44 +0700

    finish_commit

--- Working_Files.py
+++ Working_Files.py
@@ -19,23 +19,29 @@ for i in cmd:
     print(i)
     sp=subprocess.Popen(i, shell= True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines= True)   
     rc=sp.wait()
-    out,err=sp.communicate(input=sp,timeout= 5000)
+    out,err=sp.communicate()
     print(f'rc: {rc}\n')
     print(f"out: {out.splitlines()}\n")
     print(f"err: {err.splitlines()}")
+
 '''
-'''
+
+
+
+
 sp1=subprocess.run(r"cd /d D:\Python_Ex\Working_Files\Working_Files\test-github", shell= True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines= True) 
 print(sp1.stderr)
-sp2=subprocess.run("git add .", shell= True,stdin=sp1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines= True) 
+sp2=subprocess.run("git add .", shell= True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines= True) 
 print(sp2.stderr)
-sp3=subprocess.run("git commit -am \"finish_commit\"", shell= True,stdin=sp2.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines= True) 
+sp3=subprocess.run("git commit -am \"finish_commit\"", shell= True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines= True) 
 print(sp3.stderr)
 #sp4=subprocess.run("git remote add origin \"https://github.com/trankhai677/test-github.git \"", shell= True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines= True) 
 #print(sp4.stderr)
-sp5=subprocess.run("git push origin main", shell= True, stdout=subprocess.PIPE,stdin=sp3.stdout, stderr=subprocess.PIPE, universal_newlines= True) 
+sp5=subprocess.run("git push origin main", shell= True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines= True) 
 print(sp5.stderr)
+
 '''
 os.system("git add .")
 os.system("git commit -am \"finish-commit\" ")
 os.system("git push origin main") 
+'''
