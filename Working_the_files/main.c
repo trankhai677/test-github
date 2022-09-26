bdbaf5506e2ee2c99e9cad7bcbb8bd12e6d19ffa
trankhai677 <72657994+trankhai677@users.noreply.github.com>
2022-09-26 11:37:35 +0700

    Delete Working_the_files directory

--- Working_the_files/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(void) 
-{
-	printf("Hello, This is  inside C Language.");
-	return 0;
-}
--- Working_the_files/main_1.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-print("Inside Python file.");
-subprocess.call(["gcc","main.c"])
-subprocess.call("./a.exe");
-print("Task is done.")
